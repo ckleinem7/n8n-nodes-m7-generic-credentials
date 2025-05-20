c349208156dd70bfb12f3d3be9726970e72c91c4
Christoph Kleine <christophkleine@mount7.com>
2025-05-20 10:45:09 +0200

    Add dist folder
    - Workaround for not publishing to npm

---
+++ dist/credentials/M7GenericOAuth2Api.credentials.d.ts
@@ -0,0 +1,11 @@
+import type { IAuthenticateGeneric, ICredentialDataDecryptedObject, ICredentialType, IHttpRequestHelper, INodeProperties } from 'n8n-workflow';
+export declare class m7GenericOAuth2Api implements ICredentialType {
+    name: string;
+    displayName: string;
+    documentationUrl: string;
+    properties: INodeProperties[];
+    preAuthentication(this: IHttpRequestHelper, credentials: ICredentialDataDecryptedObject): Promise<{
+        sessionToken: string;
+    }>;
+    authenticate: IAuthenticateGeneric;
+}
---
+++ dist/credentials/M7GenericOAuth2Api.credentials.js
@@ -0,0 +1,82 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.m7GenericOAuth2Api = void 0;
+class m7GenericOAuth2Api {
+    constructor() {
+        this.name = 'm7GenericOAuth2Api';
+        this.displayName = 'M7 Generic OAuth2 API';
+        this.documentationUrl = 'httprequest';
+        this.properties = [
+            {
+                displayName: 'Session Token',
+                name: 'sessionToken',
+                type: 'hidden',
+                typeOptions: {
+                    expirable: true,
+                    password: true,
+                },
+                default: '',
+            },
+            {
+                displayName: 'Token Url',
+                name: 'tokenUrl',
+                type: 'string',
+                required: true,
+                default: 'https://your-domain.eu.auth0.com/oauth/token',
+            },
+            {
+                displayName: 'Audience',
+                name: 'audience',
+                type: 'string',
+                required: false,
+                default: '',
+            },
+            {
+                displayName: 'Client ID',
+                name: 'clientId',
+                type: 'string',
+                required: true,
+                default: '',
+            },
+            {
+                displayName: 'Client Secret',
+                name: 'clientSecret',
+                type: 'string',
+                typeOptions: {
+                    password: true,
+                },
+                required: true,
+                default: '',
+            }
+        ];
+        this.authenticate = {
+            type: 'generic',
+            properties: {
+                headers: {
+                    Authorization: '=Bearer {{$credentials.sessionToken}}',
+                },
+            },
+        };
+    }
+    async preAuthentication(credentials) {
+        const body = {
+            client_id: credentials.clientId.toString(),
+            client_secret: credentials.clientSecret.toString(),
+            grant_type: 'client_credentials',
+        };
+        if (credentials.audience) {
+            body.audience = credentials.audience.toString();
+        }
+        const { access_token } = (await this.helpers.httpRequest({
+            method: 'POST',
+            url: credentials.tokenUrl.toString(),
+            body: new URLSearchParams(body),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        }));
+        return { sessionToken: access_token };
+    }
+}
+exports.m7GenericOAuth2Api = m7GenericOAuth2Api;
+//# sourceMappingURL=M7GenericOAuth2Api.credentials.js.map
